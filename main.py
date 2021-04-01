651740b2021806014aa1011763546ad76491320c
Rocketman17 <kadekarswapnil17@gmail.com>
2021-03-31 22:42:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -175,6 +175,7 @@ def predict_file():
         #return render_template('home.html')
     #return redirect('/home')
     print("files Deleted from delete_files")
+    return redirect ('/home')
 
 
 
